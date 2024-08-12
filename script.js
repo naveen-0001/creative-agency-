48c38fd8b4cf708b5951cede931d05d9c26036b7
naveen jangid <119118768+naveen-0001@users.noreply.github.com>
2024-08-12 19:36:11 +0530

    Add files via upload
    creative project using html css js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const toggleButton = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
+
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+navigation.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
